e0134fbb83cf04df8261ee84f0916bb75ff1402b
BShreevas <77844431+BShreevas@users.noreply.github.com>
2021-03-21 20:33:51 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,24 @@
+class Box{
+    constructor(x){
+        var options={
+            restitution:0.8,
+            friction:1,
+            density:20
+        }
+        this.body=Bodies.rectangle(x,100,70,70,options);
+        this.width=70;
+        this.height=70;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos=this.body.position;
+        var angle=this.body.angle;
+        push()
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill("red");
+        rect(0,0,this.width,this.height);
+        pop()
+    }
+}
---
+++ hero.js
@@ -0,0 +1,26 @@
+class Hero {
+	
+    constructor(x,y,r)
+      {
+          var options = { 
+              density: 1, 
+              frictionAir: 1
+      }
+          this.r=r;
+          this.image=loadImage("Superhero-01.png");
+          this.body=Bodies.circle(x,y,(this.r)/2, options);
+          World.add(world, this.body);
+  
+      }
+      display()
+      {
+              var Pos=this.body.position;		
+              push()
+              translate(Pos.x,Pos.y-100);
+              fill(255,0,255);
+              imageMode(CENTER);
+              image(this.image,0,0,this.r+150, this.r);
+              pop();		
+      }
+  }
+  
